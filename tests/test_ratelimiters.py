56011ec719608a8e00a7bdcd68b3fc1750493170
Paaksing <paaksingtech@gmail.com>
2024-10-28 23:54:56 -0400

    ci: fix subprocess port issues

--- tests/test_ratelimiters.py
+++ tests/test_ratelimiters.py
@@ -1,7 +1,6 @@
 import asyncio
 import os
 import subprocess
-import time
 
 import aiohttp
 
@@ -16,15 +15,15 @@ from pulsefire.ratelimiters import RiotAPIRateLimiter
 from pulsefire.taskgroups import TaskGroup
 
 
-RATELIMITER_PROXY_SCRIPT = '''
-from pulsefire.ratelimiters import RiotAPIRateLimiter
-RiotAPIRateLimiter().serve()
-'''.strip()
+RATELIMITER_PROXY_SCRIPT = (
+    "from pulsefire.ratelimiters import RiotAPIRateLimiter;"
+    "RiotAPIRateLimiter().serve()"
+)
 
-RATELIMITER_PROXY_SECRET_SCRIPT = '''
-from pulsefire.ratelimiters import RiotAPIRateLimiter
-RiotAPIRateLimiter().serve(secret='sAmPLesECReT')
-'''.strip()
+RATELIMITER_PROXY_SECRET_SCRIPT = (
+    "from pulsefire.ratelimiters import RiotAPIRateLimiter;"
+    "RiotAPIRateLimiter().serve(secret='sAmPLesECReT')"
+)
 
 @async_to_sync()
 async def test_riot_api_rate_limiter_local():
@@ -44,60 +43,67 @@ async def test_riot_api_rate_limiter_local():
 
 @async_to_sync()
 async def test_riot_api_rate_limiter_proxy():
-    popen = subprocess.Popen(f'python -c "{RATELIMITER_PROXY_SCRIPT}"', shell=True)
+    cmd = ["python", "-c", f'"{RATELIMITER_PROXY_SCRIPT}"']
+    popen = subprocess.Popen(" ".join(cmd) if os.name == 'nt' else cmd)
+    try:
+        await asyncio.sleep(1)
 
-    async with RiotAPIClient(
-        default_headers={"X-Riot-Token": os.environ["RIOT_API_KEY"]},
-        middlewares=[
-            json_response_middleware(),
-            http_error_middleware(),
-            rate_limiter_middleware(RiotAPIRateLimiter(proxy="http://127.0.0.1:12227")),
-        ]
-    ) as client:
-        async with TaskGroup(asyncio.Semaphore(100)) as tg:
-            for _ in range(70):
-                await tg.create_task(client.get_lol_champion_v3_rotation(region="na1"))
-        assert tg.results()
-
-    popen.kill()
+        async with RiotAPIClient(
+            default_headers={"X-Riot-Token": os.environ["RIOT_API_KEY"]},
+            middlewares=[
+                json_response_middleware(),
+                http_error_middleware(),
+                rate_limiter_middleware(RiotAPIRateLimiter(proxy="http://127.0.0.1:12227")),
+            ]
+        ) as client:
+            await client.get_lol_champion_v3_rotation(region="na1")
+            async with TaskGroup(asyncio.Semaphore(100)) as tg:
+                for _ in range(2):
+                    await tg.create_task(client.get_lol_champion_v3_rotation(region="na1"))
+            assert tg.results()
+    finally:
+        popen.terminate()
 
 
 @async_to_sync()
 async def test_riot_api_rate_limiter_proxy_secret():
-    popen = subprocess.Popen(f'python -c "{RATELIMITER_PROXY_SECRET_SCRIPT}"', shell=True)
-    await asyncio.sleep(1)
+    cmd = ["python", "-c", f'"{RATELIMITER_PROXY_SECRET_SCRIPT}"']
+    popen = subprocess.Popen(" ".join(cmd) if os.name == 'nt' else cmd)
+    try:
+        await asyncio.sleep(1)
 
-    async with RiotAPIClient(
-        default_headers={"X-Riot-Token": os.environ["RIOT_API_KEY"]},
-        middlewares=[
-            json_response_middleware(),
-            http_error_middleware(),
-            rate_limiter_middleware(RiotAPIRateLimiter(
-                proxy="http://127.0.0.1:12227",
-                proxy_secret="WRONGsECReT"
-            )),
-        ]
-    ) as client:
-        try:
-            await client.get_lol_champion_v3_rotation(region="na1")
-            assert False, "Expected exception"
-        except aiohttp.ClientResponseError as e:
-            assert e.status == 401
+        async with RiotAPIClient(
+            default_headers={"X-Riot-Token": os.environ["RIOT_API_KEY"]},
+            middlewares=[
+                json_response_middleware(),
+                http_error_middleware(),
+                rate_limiter_middleware(RiotAPIRateLimiter(
+                    proxy="http://127.0.0.1:12227",
+                    proxy_secret="WRONGsECReT"
+                )),
+            ]
+        ) as client:
+            try:
+                await client.get_lol_champion_v3_rotation(region="na1")
+                assert False, "Expected exception"
+            except aiohttp.ClientResponseError as e:
+                assert e.status == 401
 
-    async with RiotAPIClient(
-        default_headers={"X-Riot-Token": os.environ["RIOT_API_KEY"]},
-        middlewares=[
-            json_response_middleware(),
-            http_error_middleware(),
-            rate_limiter_middleware(RiotAPIRateLimiter(
-                proxy="http://127.0.0.1:12227",
-                proxy_secret="sAmPLesECReT"
-            )),
-        ]
-    ) as client:
-        async with TaskGroup(asyncio.Semaphore(100)) as tg:
-            for _ in range(70):
-                await tg.create_task(client.get_lol_champion_v3_rotation(region="na1"))
-        assert tg.results()
+        async with RiotAPIClient(
+            default_headers={"X-Riot-Token": os.environ["RIOT_API_KEY"]},
+            middlewares=[
+                json_response_middleware(),
+                http_error_middleware(),
+                rate_limiter_middleware(RiotAPIRateLimiter(
+                    proxy="http://127.0.0.1:12227",
+                    proxy_secret="sAmPLesECReT"
+                )),
+            ]
+        ) as client:
+            async with TaskGroup(asyncio.Semaphore(100)) as tg:
+                for _ in range(70):
+                    await tg.create_task(client.get_lol_champion_v3_rotation(region="na1"))
+            assert tg.results()
 
-    popen.kill()
+    finally:
+        popen.terminate()
